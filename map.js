dcaee2ed540db3fc4e63792b184813f3beeae8a4
Xyresic <ericjmlam@gmail.com>
2023-09-21 10:01:40 -0400

    Add points to map

--- map.js
+++ map.js
@@ -4,11 +4,11 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: 'red',
-    fillOpacity: 1,
-    radius: 100
-}).addTo(map);
-
-let locations = []
+for (let location of locations) {
+    L.circle(location, {
+        color: 'red',
+        fillColor: 'red',
+        fillOpacity: 1,
+        radius: 100
+    }).addTo(map);
+}
