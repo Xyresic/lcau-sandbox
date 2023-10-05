ed37aa8cba56aca1f0101695aa1e836dcee7bfdd
Xyresic <ericjmlam@gmail.com>
2023-10-05 05:13:25 -0400

    Update filtering

--- filter.py
+++ filter.py
@@ -11,17 +11,20 @@ for city in cities_full:
     if key in cities:
         cities[key].add(city[-1])
 
-cities = {city: {'docs': list(docs)} for city, docs in cities.items()}
+cities = {city: {'docs': sorted(list(docs))} for city, docs in cities.items()}
 with open('serialized.js', 'r') as f:
     locations = eval(f.read()[16:-1].replace('`', '"'))
 
+new_locations = []
 for location in locations:
     key = location[2]
     if key in cities:
         cities[key]['lat'] = location[0]
         cities[key]['long'] = location[1]
-    else:
-        print(key)
+        new_locations.append(tuple(location))
 
 with open('cities.json', 'w') as f:
     json.dump(cities, f, indent=2)
+
+with open('serialized.js', 'w') as f:
+    f.write(f'let locations = [{", ".join([f"[{lat}, {long}, `{city}`]" for lat, long, city in new_locations])}];')
